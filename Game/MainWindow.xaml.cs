df07eff28570bd9099d72d2f257910d929b3b2a9
Yehor Ovseukov <ki201_oeyu@student.ztu.edu.ua>
2023-03-03 18:24:06 +0200

    Add project files.

---
+++ Game/MainWindow.xaml.cs
@@ -0,0 +1,68 @@
+﻿using Game.UserControls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    
+    enum CheckerType
+    {
+        None,
+        Black,
+        White
+    }
+    public partial class MainWindow : Window
+    {
+        private CheckerType[,] checkers = {
+            {CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black},
+            {CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None},
+            {CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black},
+            {CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None, CheckerType.Black, CheckerType.None},
+            {CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None},
+            {CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None, CheckerType.None},
+            {CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White},
+            {CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None},
+            {CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White},
+            {CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None, CheckerType.White, CheckerType.None},
+        };
+        public MainWindow()
+        {
+            InitializeComponent();
+            InitializeField();
+        }
+
+        private void InitializeField()
+        {
+            for (int x = 0; x < checkers.GetLength(0); x += 1)
+            {
+                for (int y = 0; y < checkers.GetLength(1); y += 1)
+                {
+                    if(checkers[x,y] != CheckerType.None)
+                    {
+                        var newChecker = new CheckerUserControl();
+                        var color = checkers[x, y] == CheckerType.White ? Color.FromRgb(255, 255, 255) : Color.FromRgb(0, 0, 0);
+                        newChecker.Brush = new SolidColorBrush(color);
+                        Grid.Children.Add(newChecker);
+                        Grid.SetRow(newChecker, x+1);
+                        Grid.SetColumn(newChecker, y+1);
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ Game/UserControls/CheckerUserControl.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Game.UserControls
+{
+    /// <summary>
+    /// Interaction logic for CheckerUserControl.xaml
+    /// </summary>
+    public partial class CheckerUserControl : UserControl
+    {
+        public CheckerUserControl()
+        {
+            InitializeComponent();
+            this.DataContext = this;
+        }
+
+        public Brush Brush { get; set; } = new SolidColorBrush(Color.FromRgb(0,0,0));
+    }
+}
